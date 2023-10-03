6939b4829d8cc22988668ce746bfbb9e985490d5
ntminh181294 <ntminh181294@gmail.com>
2023-10-03 19:17:25 +0700

    Add project files.

---
+++ test001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 6; // Số dòng (số hàng) của hình
+
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("$");
+            }
+            Console.WriteLine(); // Xuống dòng sau khi in mỗi dòng
+        }
+    }
+}
