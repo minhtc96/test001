e6b9272dd0e1adc80b70d741b1c4469cc92cc3b5
ntminh181294 <ntminh181294@gmail.com>
2023-10-05 19:39:53 +0700

    tes1

---
+++ test001/ConsoleApp27/ConsoleApp27/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        
+        int n, k;
+        bool check = false;
+        Console.WriteLine("Nhap so phan tu n: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        int[] a = new int[n] ;
+        Console.WriteLine("Nhap gia tri cho tung phan tu: ");
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("Nhap so k: ");
+        k = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == k)
+            {
+                Console.WriteLine(a[i]);
+                Console.WriteLine("Vi tri cua a[{0}] la {1}", i, k);
+                check = true;
+            }
+            
+        }
+        if (!check)
+        {
+            Console.WriteLine("Khong co gia tri nao trong mang trung voi gia tri k da cho");
+        }
+        Console.ReadLine();
+    }
+}
